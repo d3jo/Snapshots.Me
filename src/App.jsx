8388d00a98129530a7f7bcabf5452cff9b89898d
Ajun Jo <joajune03@gmail.com>
2024-07-16 11:02:08 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Contact from './components/Contact';
 
 
 
+
 const App = () => {
     return (
         <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
