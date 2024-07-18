c52f86839ffe99bbdcb6d24addc8ba8d2c752875
d3jo <132111210+d3jo@users.noreply.github.com>
2024-07-17 20:05:31 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Snapshots.Me/",
   plugins: [react()],
-  base: '/Snapshots.Me/'
-});
+})
